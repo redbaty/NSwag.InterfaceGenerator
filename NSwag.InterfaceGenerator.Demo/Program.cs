428673a41a0a2b22d8cabb2dff5cd7b2b7b77148
Marcos Vinicius Maia <juninhowblu@gmail.com>
2018-05-13 18:40:29 -0300

    Added demo project

---
+++ NSwag.InterfaceGenerator.Demo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NSwag.InterfaceGenerator.Builders;
+
+namespace NSwag.InterfaceGenerator.Demo
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            new SwaggerInterfaceBuilder().WithUrl("http://localhost:5000/swagger/v1/swagger.json").Build().Wait();
+            Console.ReadKey();
+        }
+    }
+}
